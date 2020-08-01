9dc52e3203779b838986fa9384ec1f3dddb50b64
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-07-31 22:49:45 -0500

    first commit for example arrays np with numba

---
+++ sum_matrix_numba.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Decorador @vectorize para generar una versión compilada y vectorizada de la función escalar “Add” en tiempo de ejecución,
+a fin de que pueda usarse para procesar matrices de datos en paralelo en la GPU.
+
+Requisitos:
+Python 3.7.6
+Conda 4.8.2
+
+Install:
+
+$> conda install numba, cudatoolkit
+
+Información:
+Python Compiler : MSC v.1916 64 bit (AMD64)
+LLVM version : 8.0.0
+numba: 0.48.0
+"""
+# from MachineInfo import MachineInfo
+import numpy as np
+
+if __name__ == "__main__":
+    # Inicializando Arrays
+    N = 100
+    A = np.ones(N, dtype=np.float32)
+    print("A")
+    print(A)
+    B = np.ones(A.shape, dtype=A.dtype)
+    print("B")
+    print(B)
+    C = np.empty_like(A, dtype=A.dtype)
+    print("C")
+    print(C)
+    print(np.sum(C))
+    
